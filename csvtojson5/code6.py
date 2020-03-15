6a30f5536318a120067b8f5590c3209473a9ea57
mahdi <3967516+computerholic@users.noreply.github.com>
2020-03-15 05:02:38 -0400

    issue #349 detectron fixed.

--- csvtojson5/code6.py
+++ csvtojson5/code6.py
@@ -14,6 +14,9 @@ categories[{
 from detectron2.data.datasets.coco import convert_to_coco_json
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
+from detectron2.structures import Boxes, BoxMode, PolygonMasks # NameError: name 'BoxMode' is not defined.
+
+## area should be 30*40=1200
 def data_dict():
     return [{
         'file_name': 'image.png',
@@ -22,7 +25,6 @@ def data_dict():
         'width': 100,
         'annotations': [{
             'bbox': [70, 30, 100, 70],
-            ## area should be 30*40=1200
             'bbox_mode': BoxMode.XYXY_ABS,
             'category_id': 0,
             'iscrowd': 0,
@@ -30,9 +32,9 @@ def data_dict():
     }]
 
 def to_json():
-    DatasetCatalog.register('test', data_dict)
-    MetadataCatalog.get('test').set(thing_classes=["first"])
-    convert_to_coco_json('test', output_folder='./output', allow_cached=False)
+    DatasetCatalog.register('test', data_dict) # test is already registered.
+    MetadataCatalog.get('test').set(thing_classes=["first"]) # Metadata(name='test', thing_classes=['first'])
+    convert_to_coco_json('test', output_file='./output', allow_cached=False) # output_folder -> output_file
 
 if __name__ == '__main__':
     to_json()
