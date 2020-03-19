0757871c9715085365764c439f21349fc9f5a24d
Mahdi Kashani <3967516+computerholic@users.noreply.github.com>
2020-03-19 15:09:02 -0400

    Still Working?
    Description -> Read the dataDict_output. -> Follow the example. ( Better )

--- csvtojson5/code6.py
+++ csvtojson5/code6.py
@@ -1,29 +1,51 @@
-# Implementing the data_dict() on one sample image:
-
+# Validate the detectron dataset + other stuff that can happen here.
 # Maybe later: # http://cocodataset.org/#format-data
 
-# NameError: name 'BoxMode' is not defined.
+# NameError: name 'BoxMode' is not defined. -> python setup.py install <- detectron2.
+
 from detectron2.structures import Boxes, BoxMode, PolygonMasks
-from detectron2.structures import BoxMode, PolygonMasks, Boxes
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.datasets.coco import convert_to_coco_json
 
 
-def read_csv():
-    import pandas as pd
-    # Read a comma-separated values (.csv) file into DataFrame.
-    # From code 1.
-    DF = pd.read_csv('../Data/Annotations/FL_Keys_Coral-export.csv')
-    return DF
+def data_dict0():
+    """
+    generated from code7.py. However, is not compatible with the 
+    25, in convert_to_coco_json
+    coco_dict = convert_to_coco_dict(dataset_name)
+  File "/Users/mac7/opt/anaconda3/envs/myenvpy/lib/python3.7/site-packages/detectron2-0.1.1-py3.7-macosx-10.9-x86_64.egg/detectron2/data/datasets/coco.py", line 314, in convert_to_coco_dict
+    "width": image_dict["width"],
+    KeyError: 'width'
+    """
+
+    # 0- Sample from detectron2 -> 5 different sections.
+    info_val0 = [{"date_created": "2020-03-15 04:59:45.442988",
+                  "description": "Automatically generated COCO json file for Detectron2."}]
+    images0 = [{"id": "image", "width": 100,
+                "height": 100, "file_name": "image.png"}]
+    annotations0 = [{"id": 1, "image_id": "image", "bbox": [70.0, 30.0, 30.0, 40.0],
+                     "area": 1200.0, "iscrowd": 0, "category_id": 0}]
+    categories0 = [{"id": 0, "name": "first"}]
+    licence0 = 'null'
+
+    return [{"info": info_val0,
+             "images": images0,
+             "annotations": annotations0,
+             "categories": categories0,
+             "licenses": licence0}]
 
 
-DF = read_csv()
-
-# I took it from:
-# https://github.com/facebookresearch/detectron2/issues/349
+# print(data_dict0(), '\n\n\n') # test above function.
 
 
+# Implementing the data_dict() on one sample image:
 def data_dict():
+    """
+    This is just a sample dictionary to test the detectron.
+    I took it from:
+    https://github.com/facebookresearch/detectron2/issues/349
+    """
+    print("\ndata_dict(): ", "Finished successfully!")
     return [{
         'file_name': 'image.png',
         'image_id': 'image',
@@ -38,68 +60,102 @@ def data_dict():
     }]
 
 
-""" ################################################################ You can add the add later.
+print(data_dict(), '\n\n\n\n')
+
+
 def to_json():
-    DatasetCatalog.register('test', data_dict) # test is already registered.
-    MetadataCatalog.get('test').set(thing_classes=["first"]) # Metadata(name='test', thing_classes=['first'])
-    convert_to_coco_json('test', output_file='./output', allow_cached=False) # output_folder -> output_file
+    # test is already registered.
+    DatasetCatalog.register('test', data_dict0)
+    # Metadata(name='test', thing_classes=['first'])
+    MetadataCatalog.get('test').set(thing_classes=["first"])
+    convert_to_coco_json('test', output_file='./output',
+                         allow_cached=False)  # output_folder -> output_file
+    print("to_json(): ", "Finished successfully!")
+
 
 if __name__ == '__main__':
     to_json()
-"""
+    print("__main__: Finished successfully!\n")
 
 
-def data_dict2(fn, bbox, image_id, height, width):
-    return [{
-        'file_name': fn,
-        'image_id': image_id,
-        'height': height,
-        'width': width,
-        'annotations': [{
-            'bbox': bbox,
-            'bbox_mode': BoxMode.XYXY_ABS,
-            'category_id': 0,
-            'iscrowd': 0,
-        }]
-    }]
+# CSV_PATH = "../Data/Annotations/FL_Keys_Coral-export.csv"
+# JPGPATH = '../Data/vott-csv-export/JPGImages/'
+# def makeDF(csv_path):
+#     """ Read a comma-separated values (.csv) file into DataFrame.
+#     Arg: csv PATH:
+#     Return: new DF.
+#     """
+#     DF = pd.read_csv(csv_path)
+
+#     DF['height'] = DF.apply(lambda DF: abs(DF['ymax'] - DF['ymin']), axis=1)
+#     DF['width'] = DF.apply(lambda DF: abs(DF['xmax'] - DF['xmin']), axis=1)
+#     DF['objArea'] = DF.apply(lambda DF: (DF['width'] * DF['height']), axis=1)
+#     imageArea = 2704 * 1524
+#     DF['objPortion'] = DF.apply(lambda DF: (DF['objArea'] / imageArea), axis=1)
+
+#     # DF.to_csv('/NewDF.csv')
+
+#     # Looking at the first 5 rows to get the insigt on the data.
+#     print(DF.head(5))
+#     print(DF.label.unique())
+#     print("\nMADE_DF(): Finished successfully!\n")
+#     return DF
 
 
-fn = 'image.png',
-bbox = [70, 30, 100, 70]
-image_id = 'image'
+# DF = makeDF(csv_path=CSV_PATH)
 
-""" Path and image names.
-JPGImagesPATH = './Data/vott-csv-export/JPGImages/'
-jpg1_str = 'A_3D_L0646_144.jpg'
-"""
 
-imagelist1 = ['A_3D_L0646_144.jpg', '3D_L0622_176.jpg', '3R010215_829.jpg',
-              '3D_L0622_139.jpg', 'B_3D_L0647_42.jpg']  # To do later on 5 different images.
-im_0 = ['3D_L0441_41.jpg']  # 4 objects lowest 3
+# def data_dict2(fn, bbox, image_id, height, width):
+#     return [{
+#         'file_name': fn,
+#         'image_id': image_id,
+#         'height': height,
+#         'width': width,
+#         'annotations': [{
+#             'bbox': bbox,
+#             'bbox_mode': BoxMode.XYXY_ABS,
+#             'category_id': 0,
+#             'iscrowd': 0,
+#         }]
+#     }]
 
-for item in im_0:  # In yek bar Run mishe -> 1 ax darim -> 4 ta object tooshe -> im_0
-    # Filter the .csv file based on the ax -> behem dataframe jadid mide baraye 1 ax.
-    NewDF = DF[DF['image'] == item]
-    print(NewDF)
-    print('*'*80)
 
-    for category in [NewDF['label']]:
-        # Filter based on the object ( category ) -> category id + .
-        print(category)
+# fn = 'image.png',
+# bbox = [70, 30, 100, 70]
+# image_id = 'image'
 
- #       print(NewerDF)
+# """ Path and image names.
+# JPGImagesPATH = './Data/vott-csv-export/JPGImages/'
+# jpg1_str = 'A_3D_L0646_144.jpg'
+# """
 
-    # bbox = [NewDF.xmin, NewDF.ymin, NewDF.xmax, NewDF.ymax] # ...
-    # print(bbox)
+# imagelist1 = ['A_3D_L0646_144.jpg', '3D_L0622_176.jpg', '3R010215_829.jpg',
+#               '3D_L0622_139.jpg', 'B_3D_L0647_42.jpg']  # To do later on 5 different images.
+# im_0 = ['3D_L0441_41.jpg']  # 4 objects lowest 3
 
-    # image NAME + image FORMAT
-#    img_name = item.split('.')[0] #
-#    img_frmt = item.split('.')[1] #
+# for item in im_0:  # In yek bar Run mishe -> 1 ax darim -> 4 ta object tooshe -> im_0
+#     # Filter the .csv file based on the ax -> behem dataframe jadid mide baraye 1 ax.
+#     NewDF = DF[DF['image'] == item]
+#     print(NewDF)
+#     print('*'*80)
 
-#    print(img_name, img_frmt)
+#     for category in [NewDF['label']]:
+#         # Filter based on the object ( category ) -> category id + .
+#         print(category)
 
-    # image ID ??
-#   image_id = item[-7:-4]
+#  #       print(NewerDF)
 
-    # label ID ??
-    # To do -> create a column in pandas.
+#     # bbox = [NewDF.xmin, NewDF.ymin, NewDF.xmax, NewDF.ymax] # ...
+#     # print(bbox)
+
+#     # image NAME + image FORMAT
+# #    img_name = item.split('.')[0] #
+# #    img_frmt = item.split('.')[1] #
+
+# #    print(img_name, img_frmt)
+
+#     # image ID ??
+# #   image_id = item[-7:-4]
+
+#     # label ID ??
+#     # To do -> create a column in pandas.
--- csvtojson5/code7.py
+++ csvtojson5/code7.py
@@ -1,43 +1,95 @@
-# Should be merged with code6.py
+# Should be merged with code6.py.
 # Just trying to go through 1 image and see the pattern.
-"""
-               image         xmin         ymin         xmax         ymax    label      height       width       objArea  objPortion
-366  3D_L0441_41.jpg  1771.904437   922.258702  1984.163823  1184.253859     Ssid  261.995157  212.259386  55610.931154    0.013495
-367  3D_L0441_41.jpg  1336.311263    93.837394  1687.000683   274.650953  Antillo  180.813559  350.689420  63409.402210    0.015387
-368  3D_L0441_41.jpg  1395.374744  1147.353133  1519.038908  1304.181220  Antillo  156.828087  123.664164  19394.014263    0.004706
-369  3D_L0441_41.jpg  1194.189761   483.399516  1380.608874   758.050469  Antillo  274.650953  186.419113  51200.187013    0.012425
-"""
 
-sample_output = {"info": {"date_created": "2020-03-15 04:59:45.442988", "description": "Automatically generated COCO json file for Detectron2."},
-                 "images": [{"id": "image", "width": 100, "height": 100, "file_name": "image.png"}],
-                 "annotations": [{"id": 1, "image_id": "image", "bbox": [70.0, 30.0, 30.0, 40.0], "area": 1200.0, "iscrowd": 0, "category_id": 0}],
-                 "categories": [{"id": 0, "name": "first"}],
-                 "licenses": null}
+CSV_PATH = "../Data/Annotations/FL_Keys_Coral-export.csv"
+JPGPATH = '../Data/vott-csv-export/JPGImages/'
+
+
+def makeDF(csv_path):
+    """ Read a comma-separated values (.csv) file into DataFrame.
+    Arg: csv PATH
+    Displays the first 5 rows.
+    Return: new_DF
+    """
+    import pandas as pd
+    DF = pd.read_csv(csv_path)
+
+    DF['height'] = DF.apply(lambda DF: abs(DF['ymax'] - DF['ymin']), axis=1)
+    DF['width'] = DF.apply(lambda DF: abs(DF['xmax'] - DF['xmin']), axis=1)
+    DF['objArea'] = DF.apply(lambda DF: (DF['width'] * DF['height']), axis=1)
+    imageArea = 2704 * 1524
+    DF['objPortion'] = DF.apply(lambda DF: (DF['objArea'] / imageArea), axis=1)
+
+    # DF.to_csv('/NewDF.csv').
+
+    # Looking at the first 5 rows to get the insigt on the data.
+    print(DF.head(5))
+    print('\nLabels: ', DF.label.unique())
+    print("\nMADE_DF(): Finished successfully!\n", 130*'*')
+    return DF
+
+
+DF = makeDF(csv_path=CSV_PATH)
+
+im_0 = '3D_L0441_41.jpg'  # 4 objects lowest 3.
+im = im_0
+print(DF[DF['image'] == im])  # test.
+
+# Example-0 : A coco dataset should have 5 different segments.
+info_val0 = {"date_created": "2020-03-15 04:59:45.442988",
+             "description": "Automatically generated COCO json file for Detectron2."}
+images0 = [{"id": "image", "width": 100,
+            "height": 100, "file_name": "image.png"}]
+annotations0 = [{"id": 1, "image_id": "image", "bbox": [70.0, 30.0, 30.0, 40.0],
+                 "area": 1200.0, "iscrowd": 0, "category_id": 0}]
+categories0 = [{"id": 0, "name": "first"}]
+licence0 = 'null'
+
+sample_output = {"info": info_val0,
+                 "images": images0,
+                 "annotations": annotations0,
+                 "categories": categories0,
+                 "licenses": licence0}
+print(sample_output, 'n\n\n\n\n')
 
 # 1- "info" section: contain high level information about the dataset. # Done. (Whatever is appropriate.)
-"info": [{
+info_VAL = {
     "description": "Coral REEF Dataset",
     "url": "hopkinsonlab.org",
     "version": "Version 1.0",
     "year": "2019",
     "contributor": "Hopkinson Lab",
-    "date_created" "2019-9-3 2:07 PM",
-}
-]
+    "date_created": "2019-9-3 2:07 PM", }
+sample_infoDone = {"info": info_VAL,
+                   "images": images0,
+                   "annotations": annotations0,
+                   "categories": categories0,
+                   "licenses": licence0}
+print(sample_infoDone, '\n\n\n\n')
 
 # 2- images
 # "coco_url": null
 # "flickr_url": null
 
 # it will repeat 1 time.
-"images": [{
-    "id":
-    "licence":
-    "width": DF.['width']
-    "file_name": DF['image']
-    "date_captured":}
-]
+images_VAL = [{
+    "id": "?idImage",
+    "licence": "?licenceImage",
+    "width": DF.loc[[366, 'width']],
+    "file_name": DF.loc[366, 'image'],
+    "date_captured":'?date_capturedImage'}]
+sample_infoDone = {"info": info_VAL,
+                   "images": images_VAL,
+                   "annotations": [{"id": 1, "image_id": "image", "bbox": [70.0, 30.0, 30.0, 40.0], "area": 1200.0, "iscrowd": 0, "category_id": 0}],
+                   "categories": [{"id": 0, "name": "first"}],
+                   "licenses": 'null'}
+print(sample_infoDone, '\n\n\n\n')
 
+# for index ...
+# category_id = index
+
+
+"""
 # 3- annotations
 # "iscroud" : 0, # ------------------> I have a code to calculate it. (later project.)
 # "segmentation": [[1, 1, 2, 2, 3, 3, 4, 4][9, 9, 8, 8, 7, 7, 6, 6]],
@@ -75,3 +127,26 @@ for id in image_id:
     "url": "s3-bucket"
     "id": id,
     "name": "University of Georgia Licence"
+"""
+
+
+# Sample images to check my function.
+im_0 = '3D_L0441_41.jpg'  # 4 objects lowest 3
+# Almost 53 objects which are mostly `Galaxaura`.
+im_1 = 'A_3D_L0646_144.jpg'
+
+imagelist1 = ['A_3D_L0646_144.jpg', '3D_L0622_176.jpg',
+              '3R010215_829.jpg', '3D_L0622_139.jpg', 'B_3D_L0647_42.jpg']
+
+im = im_0
+
+NewDF = DF[DF['image'] == im]
+# NewDF.to_json('json_annotation.json')  # test.
+print(NewDF)
+
+# How many classes do we have in label?
+print(NewDF['label'].value_counts()[:])
+''' result:
+    Antillo    3
+    Ssid       1
+    '''
