483b0b38d9f878981f1807ccb87589fc4b9ef862
Mahdi Kashani <3967516+computerholic@users.noreply.github.com>
2020-04-05 21:01:32 -0400

    Add #TODO: and #FIXIT + refactoring.

--- code12.py
+++ code12.py
@@ -35,27 +35,37 @@ def makeDF(csv_path):
 
     # Looking at the first 5 rows to get the insigt on the data.
     print(DF.head(5))
-    print(DF.label.unique())
+    print(DF.info())
     return DF
 
 
-DF = makeDF(csv_path=CSV_PATH)
+DF = makeDF(csv_path=CSV_PATH)  # Test.
 
 """
 # Code 2:
 # Author: Mahdi Kashani
 # mahdi@uga.edu
 # Title: # reading single image using imread function of matplotlib
-
 """
-JPGPATH = './Data/vott-csv-export/JPGImages/'  # Used in code 3
-jpg1_str = '3D_L0215_161.jpg'  # Used in code 3
+JPGPATH = './Data/vott-csv-export/JPGImages/'
 
-jpgfile = Image.open(str(JPGPATH)+str(jpg1_str))  # .convert('RGBA')
+
+def _sortedImages():
+
+    return pd.DataFrame(DF['image'].value_counts()).reset_index()['index'].to_numpy(dtype=list)
+
+
+top_5_list = _sortedImages()[0:5]
+top_5to10_list = _sortedImages()[5:10]
+top_10to15_list = _sortedImages()[10:15]
+# TODO: Reading a random image from the above directory or just reading the first image.
+jpg1_str = '3D_L0215_161.jpg'
+
+jpgfile = Image.open(str(JPGPATH)+str(jpg1_str))  # .convert('RGBA') Code4.py
 
 # Printing the size, shape and the format of each image. (in bits, (width,length), jpeg/png/jpg ... ?
-print(jpgfile.bits, '= bits |', jpgfile.size,
-      '= shape |', jpgfile.format, '= imageType')
+print(jpgfile.bits, '= bits ||', jpgfile.size,
+      '= shape ||', jpgfile.format, '= imageType')
 # jpgfile.show()
 
 # How many images do I have?
@@ -64,17 +74,11 @@ print(DF['image'].nunique())
 # How many objects in each images exist?
 print(DF['image'].value_counts())
 
-num = 20
-pd.DataFrame(DF['image'].value_counts().head(num)).to_csv(
-    'top_'+str(num)+'.csv')  # Small fix is needed here. (later)
 
-print(DF['image'].value_counts().head(20))
-top_5_list = ['A_3D_L0646_144.jpg', '3D_L0622_176.jpg',
-              '3R010215_829.jpg', 'B_3D_L0647_42.jpg', '3D_L0622_139.jpg']
+print(top_10to15_list)
 
 # How many classes do we have in label?
 print(DF['label'].value_counts()[:])
-
 
 # Visualization:
 cn = ['red', 'green', 'blue', 'tan', 'magenta',
@@ -86,13 +90,14 @@ plt.xlabel('Classes')
 plt.ylabel('Numbers')
 
 # https://stackoverflow.com/questions/51058053/how-to-plot-a-histogram-of-one-column-colored-by-another-in-python
-# build a histogram for the same class breaks as earlier chart
+# Build a histogram for the same class breaks as earlier chart
 n, bins, patches = plt.hist(DF['label'], bins=len(cn))
 
-# apply the same color for each class to match the map
+# Apply the same color for each class to match the map
 idx = 0
 for c, p in zip(bins, patches):
     plt.setp(p, 'facecolor', cn[idx])
     idx += 1
 
 plt.show()
+# FIXME: Histogram needed to be right in the middle.
--- code3.py
+++ code3.py
@@ -2,97 +2,104 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont  # Explained Bellow.
 # https://pillow.readthedocs.io/en/3.1.x/reference/ImageDraw.html
-# http://effbot.org/imagingbook/introduction.html
 
-# Read a comma-separated values (.csv) file into DataFrame.
-DF = pd.read_csv('./Data/Annotations/FL_Keys_Coral-export.csv')  # From code 1.
-# print(DF.label.unique()) # From code 1.
-
-JPGImagesPATH = './Data/vott-csv-export/JPGImages/'  # From code 2.
-# jpg1_str = '3D_L0215_161.jpg' # From code 2.
+CSV_PATH = "./Data/Annotations/FL_Keys_Coral-export.csv"
+JPGImagesPATH = './Data/vott-csv-export/JPGImages/'
 
 # Color Name.
 cn = ['red', 'green', 'blue', 'tan', 'magenta',
       'black', 'white', 'cyan', 'yellow', 'teal']
 # https://python-graph-gallery.com/python-colors/
-# Different Classes.
-classes = ['Past', 'SeaRods', 'Apalm', 'Antillo', 'Other_Coral',
-           'Fish', 'Galaxaura', 'Orb', 'Gorgonia', 'Ssid']
 
-#    """
-#    # Label Dictionary
-#    lb_dic={"Past":cn[0],"Gorgonia":cn[1],'SeaRods':cn[2],'Antillo':cn[3],'Fish':cn[4],'Ssid':cn[5],'Orb':cn[6],'Other_Coral':cn[7],'Apalm':cn[8],'Galaxaura':cn[9]}
-#    print(lb_dic.keys())
-#    print(lb_dic.values())
-#    """
 
-# First 'Antillo', 'Other_Coral' -> Magenta + cyan + white + yellow + red + green
-imagelist1 = ['A_3D_L0646_144.jpg', '3D_L0622_176.jpg',
-              '3R010215_829.jpg', '3D_L0622_139.jpg', 'B_3D_L0647_42.jpg']
-imagelist2 = ['3L040215_322.jpg', 'B_3D_L0647_127.jpg',
-              '3D_L0622_91.jpg', '3D_L0622_150.jpg', '3D_L0453_49.jpg']
-imagelist3 = ['3D_L0453_283.jpg', 'B_3D_L0647_160.jpg', '3D_L0623_38.jpg',
-              '3L010215_778.jpg', 'B_3D_L0647_488.jpg']  # First 'Apalm' Observed here. -> Color Blue
-imagelist4 = ['A_3D_L0646_394.jpg', '3D_L0215_202.jpg',
-              '3D_L0623_59.jpg', 'C_3D_L0648_194.jpg', 'A_3D_L0646_263.jpg']
+def makeDF(csv_path):
+    """ Read a comma-separated values (.csv) file into DataFrame.
+    Arg: csv PATH:
+    Return: new DF.
+    """
+    DF = pd.read_csv(csv_path)
+
+    DF['height'] = DF.apply(lambda DF: abs(DF['ymax'] - DF['ymin']), axis=1)
+    DF['width'] = DF.apply(lambda DF: abs(DF['xmax'] - DF['xmin']), axis=1)
+    DF['objArea'] = DF.apply(lambda DF: (DF['width'] * DF['height']), axis=1)
+    imageArea = 2704 * 1524
+    DF['objPortion'] = DF.apply(lambda DF: (DF['objArea'] / imageArea), axis=1)
+
+    # DF.to_csv('/NewDF.csv')
+    DF.to_json('json_annot_all.json')
+
+    # Looking at the first 5 rows to get the insigt on the data.
+    print(DF.head(5))
+    print(DF.label.unique())
+    return DF
+
+
+def _boxMsg(imageList):
+    def _insideIfBoxMsg(category_id):
+        print(classes[category_id])  # tested.
+        msg = classes[category_id]
+        w, h = draw.textsize(msg)
+
+        edgecolor = cn[category_id]
+        draw.rectangle((xmin, ymin, xmax, ymax), outline=edgecolor, width=3)
+        # TODO: <Modify> the object's size -> Scale of the Box size. ? >
+        draw.text(((xmin + (width-w)/2), ymin+(height-h)/2),
+                  text=msg, fill=edgecolor)
+
+    for jpeg_str in imageList:
+        # GOAL IS: Plot the image BEFORE and AFTER the bouding Box.
+        input_image = Image.open(str(JPGImagesPATH)+str(jpeg_str))
+
+        # (outside of the loop).
+        # input_image.show(title = "input image")
+        # Image.save(output_image,"input_image") # https://stackoverflow.com/questions/26379027/using-pillows-image-save-function-throws-an-attributeerror-when-trying-to-dow
+
+        """ Visualizing it later on.
+        font_size = 14
+        font = ImageFont.truetype("arial.ttf", font_size)
+        """
+
+        draw = ImageDraw.Draw(input_image)
+
+        # _ is the object number, for example in 3D_L0215_161.jpg we have 29 objects.
+        for obj_id, row in DF[DF.image == jpeg_str].iterrows():
+
+            xmin = row.xmin
+            xmax = row.xmax
+            ymin = row.ymin
+            ymax = row.ymax
+
+            width = row.xmax - row.xmin
+            height = row.ymax - row.ymin
+            print('object_id: '+str(obj_id + 2), "width: " +
+                  str(width), "height: "+str(height))
+
+            for category_id in range(len(classes)):
+                if row.label == classes[category_id]:
+                    _insideIfBoxMsg(category_id)
+
+        # (outside of the loop) Ploting the image after bouding box.
+        output_image = input_image
+        output_image.show(title="output image")
+
+        output_image.save('./BG/'+jpeg_str[:-4]+'_BGtruth.jpeg')
+
+
+def _sortedImages():
+    return pd.DataFrame(DF['image'].value_counts()).reset_index()['index'].to_numpy(dtype=list)
+
 
 down5 = ['3D_L0443_21.jpg', '3D_L0443_36.jpg',
          '3D_L0441_41.jpg', '3D_L0457_97.jpg', '3D_L0453_119.jpg']
 
-imagelist = down5
+if __name__ == '__main__':
+    DF = makeDF(csv_path=CSV_PATH)  # Test.
+    # print(DF.label.unique()) # From code 1. #FIXME: IT's not sorted as the Detectron2 is sorted.
 
-print(imagelist)  # Test
+    # Different Classes copied from code! -> Detectron2.
+    classes = ['Past', 'Gorgonia', 'SeaRods', 'Antillo', 'Fish',
+               'Ssid', 'Orb', 'Other_Coral', 'Apalm', 'Galaxaura']
 
-
-def _insideIfBoxMsg(category_id):
-    print(classes[category_id])  # tested.
-    msg = classes[category_id]
-    w, h = draw.textsize(msg)
-
-    edgecolor = cn[category_id]
-    draw.rectangle((xmin, ymin, xmax, ymax), outline=edgecolor, width=3)
-    draw.text(((xmin + (width-w)/2), ymin+(height-h)/2),
-              text=msg, fill=edgecolor)
-
-
-for jpeg_str in imagelist:
-    jpg1_str = jpeg_str
-
-    # GOAL IS: Plot the image BEFORE and AFTER the bouding Box.
-    input_image = Image.open(str(JPGImagesPATH)+str(jpg1_str))
-
-    # (outside of the loop).
-    #input_image.show(title = "input image")
-    # Image.save(output_image,"input_image") # https://stackoverflow.com/questions/26379027/using-pillows-image-save-function-throws-an-attributeerror-when-trying-to-dow
-
-    """ Visualizing it later on.
-    font_size = 14
-    font = ImageFont.truetype("arial.ttf", font_size)
-    """
-
-    draw = ImageDraw.Draw(input_image)
-
-    # Iterating over the img for multiple objects.
-    _ = 0
-    # _ is the object number, for example in 3D_L0215_161.jpg we have 29 objects.
-    for _, row in DF[DF.image == jpg1_str].iterrows():
-
-        xmin = row.xmin
-        xmax = row.xmax
-        ymin = row.ymin
-        ymax = row.ymax
-
-        width = row.xmax - row.xmin
-        height = row.ymax - row.ymin
-        print('object_id: '+str(_), "width: " +
-              str(width), "height: "+str(height))
-
-        for category_id in range(len(classes)):
-            if row.label == classes[category_id]:
-                _insideIfBoxMsg(category_id)
-
-    # (outside of the loop) Ploting the image after bouding box.
-    output_image = input_image
-    output_image.show(title="output image")
-
-    output_image.save(jpg1_str+'_a.jpeg')
+    im = _sortedImages()[:]
+    # im = down5
+    _boxMsg(imageList=im)
